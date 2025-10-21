df6576380310fdd5806d7d939d205f522d1a8b25
Trevor Nance <trevornance7@gmail.com>
2025-10-21 12:16:20 -0400

    fixed ui - added header

--- notes/urls.py
+++ notes/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "notes"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("save/", views.save, name="save"),
-    path("new_note/", views.new_note, name="new_note"),
+    path("/save", views.save, name="save"),
+    path("/new", views.new_note, name="new_note"),
 ]
